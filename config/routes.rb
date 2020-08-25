6d18172134b6999068b68fad97161902ab0634b2
jean pierre tshimanga <mail.jp@protonmail.com>
2020-08-25 10:31:17 +0200

    routes, and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  
+
+  resources :business_cards do 
+    resources :collected_card, only: [:create]
+  end
+
+  resources :collected_cards, only: [:show, :index, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
