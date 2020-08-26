789b5c4b755eee5d57fcea6abda5804d21d1c0c9
Jaber AlAdawi <jaberadawi@gmail.com>
2020-08-26 17:55:25 +0200

    functioning show for CollectedCards

--- app/controllers/collected_cards_controller.rb
+++ app/controllers/collected_cards_controller.rb
@@ -12,4 +12,7 @@ class CollectedCardsController < ApplicationController
 
   def create
   end
+
+  def destroy
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-BusinessCard.create(
+businesscard = BusinessCard.first_or_create(
     name: "MyCard1",
-
-    businesscard = BusinessCard.first_or_create(
-
     firstname: "Joe",
     company: "email",
     email: "joe@test.com",
@@ -19,8 +15,8 @@ BusinessCard.create(
     website: "www.joes.com",
     user_id: 1,
     lastname: "example"
-    )
-)
+  )
+
 
 BusinessCard.create(
     name: "MyCard2",
@@ -36,7 +32,8 @@ BusinessCard.create(
 
 user = User.find_by(email: "ecschulte@gmx.de")
 
-CollectedCard.create(
+
+CollectedCard.create!(
   user: user,
-  business_card: businesscard,
+  business_card: businesscard
 )
