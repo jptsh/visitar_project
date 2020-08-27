7b1b6bd34d2eaa2981f9cdfa81100481635a3202
Eduard Schulte <ecschulte@gmx.de>
2020-08-27 16:08:19 +0200

    x

--- app/controllers/business_cards_controller.rb
+++ app/controllers/business_cards_controller.rb
@@ -1,6 +1,6 @@
 class BusinessCardsController < ApplicationController
   before_action :set_card, :set_vcard, only: [:show, :destroy]
-  
+
   def new
     @card = BusinessCard.new
   end
@@ -20,16 +20,15 @@ class BusinessCardsController < ApplicationController
     @vcard[:item1].label 'Other'
      @card = BusinessCard.find(params[:id])
      @enc_uri = URI.escape("#{@vcard.to_s}")
-     raise
   end
-  
+
   def create
     # @user = User.find(params[:user_id])
     @card = BusinessCard.new(card_params)
     @card.user = current_user
 
     if @card.save!
-      redirect_to business_card_path(@card)
+      redirect_to business_cards_path(@card)
     else
       render :new
     end
@@ -62,10 +61,10 @@ class BusinessCardsController < ApplicationController
   end
 
   def set_vcard
-    
+
   end
 
   def card_params
-    params.require(:business_card).permit(:firstname, :lastname, :jobtitle, :company, :phone, :email, :website)
+    params.require(:business_card).permit(:firstname, :lastname, :jobtitle, :company, :phone, :email, :website, :name)
   end
 end
