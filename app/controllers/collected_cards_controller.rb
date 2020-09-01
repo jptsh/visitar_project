7eb25d65c26cd6b5516803e50051c2da261188bb
jean pierre tshimanga <mail.jp@protonmail.com>
2020-09-01 09:09:04 +0200

    no changes

--- app/controllers/collected_cards_controller.rb
+++ app/controllers/collected_cards_controller.rb
@@ -3,7 +3,7 @@ class CollectedCardsController < ApplicationController
 
   def index
      if params[:query].present?
-      sql_query = "name @@ :query OR firstname @@ :query OR lastname @@ :query OR jobtitle @@ :query OR city @@ :query"
+       sql_query = "name @@ :query OR firstname @@ :query OR lastname @@ :query OR jobtitle @@ :query OR city @@ :query"
        @cards = CollectedCard.joins(:business_card).where(sql_query, query: "%#{params[:query]}%")
      else
        @cards = CollectedCard.all
@@ -23,6 +23,7 @@ class CollectedCardsController < ApplicationController
     qr_code = QrCode.create(data: @qr_data)
     @business_card = BusinessCard.find(qr_code[:data].split("/").last)
     @collected_card = CollectedCard.new(user_id: @business_card[:user_id], business_card_id: @business_card[:id])
+    byebug
     # @collected_card[:user_id] = @business_card[:user_id]
     # @collected_card[:business_card_id] = @business_card[:id]
     
@@ -39,6 +40,8 @@ class CollectedCardsController < ApplicationController
     @collectedcard.destroy
     redirect_to collected_cards_path
   end
+
+  private
 
   def set_qr_data
     qr_code_params = JSON.parse(params[:qr_code_json_data]).with_indifferent_access
